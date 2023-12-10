29ad1c86da0c838fbf20afbc11adc9f682378bec
Anushk Jain <anushk2201@gmail.com>
2023-12-10 19:50:36 +0530

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Counter />
     </div>
   );
 }
 
 export default App;
+
+
+const Counter = () => {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  
+
+  const handlePrevCount = () => {
+    setCount(count - step);
+    
+  }
+
+  const handleNxtCount = () => {
+    setCount(count + step);
+  }
+
+  return (
+    <div>
+      <div className='ter'>
+       <input type="range" min='1' max = '10' value={step} onChange={e => setStep(Number(e.target.value))} />
+       <h3>Step: {step}</h3>
+      </div>
+
+      <div className='ter'>
+        <button className='btn' onClick={handlePrevCount}>-</button>
+        <input type="text" value={count} onChange={e => setCount(Number(e.target.value))} />
+        <button className='btn' onClick={handleNxtCount}>+</button>
+      </div>
+
+      <div className='ter'>
+        
+        <h3>
+          <span>{count === 0 ? "Today Is " : count > 0 ? `${count} Days From Today Is ` : `${Math.abs(count)} Days Ago Was `}</span>
+          <span>{date.toDateString()}</span>
+        </h3>
+      </div>
+    </div>
+  );
+}
